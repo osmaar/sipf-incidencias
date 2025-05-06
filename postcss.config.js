e115e9991cb95000a3f616849ff7f9cd98ef4d37
igonzalezg1 <ivan.gonzales@b-process.mx>
2025-05-06 17:09:56 -0600

    Agregando plantilla base

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,14 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
 
 import autoprefixer from 'autoprefixer'
+import tailwindcss from '@tailwindcss/postcss'
+
 // import rtlcss from 'postcss-rtlcss'
 
 export default {
   plugins: [
     // https://github.com/postcss/autoprefixer
+    tailwindcss(),
     autoprefixer({
       overrideBrowserslist: [
         'last 4 Chrome versions',
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+export default {
+  prefix: 'tw-',
+  content: [
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+    './public/index.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
