8bf76f6757d89bce3f4708b7bbb1ed39680bf7aa
igonzalezg1 <ivan.gonzales@b-process.mx>
2025-05-07 15:02:59 -0600

    Finish layout

---
+++ src/pages/tecnico/IncidenciasPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <p class="tw-text-red-400">Incidencias tecnico</p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,6 +1,8 @@
 import type { RouteRecordRaw } from 'vue-router';
 import SancionesJuridicopage from 'src/pages/juridico/SancionesPage.vue';
 import SancionesTecnicoPage from 'src/pages/tecnico/SancionesPage.vue';
+import IncidenciasJuridicoPage from 'src/pages/juridico/IncidenciasPage.vue';
+import IncidenciasTecnicoPage from 'src/pages/tecnico/IncidenciasPage.vue';
 
 const routes: RouteRecordRaw[] = [
   {
@@ -16,6 +18,16 @@ const routes: RouteRecordRaw[] = [
         path: 'sanciones-juridico',
         component: SancionesJuridicopage,
         meta: { title: 'Sanciones Jurídico' },
+      },
+      {
+        path: 'incidencias-tecnico',
+        component: IncidenciasTecnicoPage,
+        meta: { title: 'Incidencias Técnico' },
+      },
+      {
+        path: 'incidencias-juridico',
+        component: IncidenciasJuridicoPage,
+        meta: { title: 'Incidencias Jurídico' },
       },
     ],
   },
