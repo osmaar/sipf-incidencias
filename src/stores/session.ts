e3fec707231c98470f866d7eef9272940c6a052b
Osmar López Islas <osmichan07@gmail.com>
2025-05-07 17:19:21 -0600

    feat: agregar store de sesión para gestionar token y datos de usuario

---
+++ src/stores/session.ts
@@ -0,0 +1,23 @@
+// src/stores/session.ts
+import { defineStore } from 'pinia';
+import type { Persona } from 'entities/persona/persona.model';
+import type { Expediente } from 'entities/expediente/expediente.model';
+
+export const useSessionStore = defineStore('session', {
+  state: () => ({
+    token: '',
+    persona: null as Persona | null,
+    expediente: null as Expediente | null,
+  }),
+  actions: {
+    setToken(token: string) {
+      this.token = token;
+    },
+    setPersona(persona: Persona) {
+      this.persona = persona;
+    },
+    setExpediente(expediente: Expediente) {
+      this.expediente = expediente;
+    },
+  },
+});
