1dfd57ded191d690b8c99b31f640e1465943d597
igonzalezg1 <ivan.gonzales@b-process.mx>
2025-05-07 13:30:08 -0600

    Finish layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
-  <q-layout view="hHh lpR fFf">
-    <q-header elevated class="bg-primary text-white" height-hint="98">
+  <q-layout view="lHh lpR fFf">
+    <q-header elevated class="tw-bg-emerald-900 text-white" height-hint="98">
       <q-toolbar>
         <q-toolbar-title>
           <div class="titulo-wrapper">
@@ -11,13 +11,52 @@
       </q-toolbar>
 
       <q-tabs align="left">
+        <q-route-tab to="/sanciones-tecnico" label="Incidencias - tecnico" />
+        <q-route-tab to="/sanciones-juridico" label="Incidencias - juridico" />
         <q-route-tab to="/sanciones-tecnico" label="Sanciones - tecnico" />
         <q-route-tab to="/sanciones-juridico" label="Sanciones - juridico" />
       </q-tabs>
     </q-header>
 
     <q-page-container>
-      <router-view />
+      <div class="row">
+        <div class="col-2">
+          <q-card class="q-ma-md q-pa-md">
+            <div class="tw-bg-emerald-900 text-center text-white">
+              ID PPL:[<span class="tw-text-green-500">997</span>]
+            </div>
+            <q-card-section class="text-center">
+              <q-avatar size="150px">
+                <img src="https://cdn.quasar.dev/img/avatar4.jpg" />
+              </q-avatar>
+              <p class="q-mt-lg tw-font-semibold tw-font-stretch-150% tw-tracking-wider">
+                Nombre de PPL
+              </p>
+              <p class="tw-font-light">29 anios de edad</p>
+            </q-card-section>
+            <div class="tw-bg-emerald-900 text-center text-white">Centro (masculino)</div>
+            <q-card-section class="text-center">
+              <p class="tw-text-green-500">CEFERESO No. 14</p>
+              <p>Tipo de Ingreso:</p>
+              <p class="tw-text-green-500">NUEVO INGRESO</p>
+              <p>Estatus: <span class="tw-text-green-500">INGRESO</span></p>
+            </q-card-section>
+            <div class="tw-bg-emerald-900 text-center text-white">Expediente actual</div>
+            <q-card-section class="text-center">
+              <p>Núm.</p>
+              <p class="tw-text-green-500">CFRS14/GP/006902/2025</p>
+              <p>Estatus: <span class="tw-text-green-500">IMPUTADO</span></p>
+            </q-card-section>
+          </q-card>
+        </div>
+        <div class="col-10">
+          <q-card class="q-ma-md q-pa-md">
+            <q-card-body>
+              <router-view />
+            </q-card-body>
+          </q-card>
+        </div>
+      </div>
     </q-page-container>
   </q-layout>
 </template>
