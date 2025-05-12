4758b8c04e8bbd7f14e8fa35ad3e4a008c11274c
Osmar López Islas <osmichan07@gmail.com>
2025-05-12 14:59:28 -0600

    Modificar la propiedad baseUrl en IncidenciasService para que sea de solo lectura

--- src/app/services/sanciones/IncidenciasService.ts
+++ src/app/services/sanciones/IncidenciasService.ts
@@ -1,9 +1,5 @@
 import { BaseService } from '../baseService';
 
 export class IncidenciasService extends BaseService {
-  private baseUrl = '/tecnico/seguridad/incidencias';
-
-  async getIncidencias(): Promise<any> {
-    return this.post(`${this.baseUrl}/paginate`);
-  }
+  private readonly baseUrl = '/tecnico/seguridad/incidencias';
 }
