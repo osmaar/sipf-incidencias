59b873eff8423cb012fb956b2129ea1cfeda5972
Osmar López Islas <osmichan07@gmail.com>
2025-05-12 14:59:17 -0600

    Eliminando rutas no utilizadas y optimizando la carga de componentes en el enrutador

--- src/app/router/routes.ts
+++ src/app/router/routes.ts
@@ -1,54 +1,25 @@
 import type { RouteRecordRaw } from 'vue-router';
-import SancionesJuridicopage from 'src/pages/juridico/SancionesPage.vue';
-import SancionesTecnicoPage from 'src/pages/tecnico/SancionesPage.vue';
-import IncidenciasJuridicoPage from 'src/pages/juridico/IncidenciasPage.vue';
-import IncidenciasTecnicoPage from 'src/pages/tecnico/IncidenciasPage.vue';
-import SancionesCreatePage from 'pages/juridico/SancionesCreatePage.vue';
-import NotLogged from 'src/pages/NotLogged.vue';
 
+// Lazy load siempre para consistencia y rendimiento
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
       {
-        path: 'sanciones-tecnico',
-        component: SancionesTecnicoPage,
-        meta: { title: 'Sanciones Técnico', requiresAuth: true },
-      },
-      {
-        path: 'sanciones-juridico',
-        component: SancionesJuridicopage,
-        meta: { title: 'Sanciones Jurídico', requiresAuth: true },
-      },
-      {
         path: 'incidencias-tecnico',
-        component: IncidenciasTecnicoPage,
+        component: () => import('pages/tecnico/IncidenciasPage.vue'),
         meta: { title: 'Incidencias Técnico', requiresAuth: true },
-      },
-      {
-        path: 'incidencias-juridico',
-        component: IncidenciasJuridicoPage,
-        meta: { title: 'Incidencias Jurídico', requiresAuth: true },
-      },
-
-      {
-        path: 'sanciones-crear',
-        component: SancionesCreatePage,
-        meta: { title: 'Incidencias Jurídico', requiresAuth: true },
       },
     ],
   },
-
-  // Always leave this as last one,
-  // but you can also remove it
   {
     path: '/not-logged',
-    component: () => NotLogged,
+    component: () => import('pages/NotLogged.vue'),
   },
   {
     path: '/:catchAll(.*)*',
-    component: () => SancionesTecnicoPage,
+    component: () => import('pages/NotLogged.vue'),
   },
 ];
 
