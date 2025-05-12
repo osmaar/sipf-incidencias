0e814e21699af7d65753eddfdb2b5a57e4a0fe60
Osmar López Islas <osmichan07@gmail.com>
2025-05-12 14:59:42 -0600

    Agregar propiedades centro y usuario al componente Header en MainLayout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
-  <q-layout view="lHh lpR fFf">
-    <HeaderComponent :titulo="titulo" />
+  <q-layout view="lHh Lpr lFf">
+    <HeaderComponent :titulo="titulo" :centro="centroActual" :user="user" />
     <q-page-container>
       <div class="row">
         <div class="col-2" v-if="isPPL">
@@ -15,19 +15,43 @@
 </template>
 
 <script setup lang="ts">
+import { ref, computed } from 'vue';
 import { useRoute } from 'vue-router';
-import { computed } from 'vue';
+import type { User } from 'src/entities/user/user.model';
+import type { Centro } from 'src/entities/centro/centro.model';
+import BodyComponent from 'src/shared/ui/layout/BodyComponent.vue';
 import HeaderComponent from 'src/shared/ui/layout/HeaderComponent.vue';
 import PPLCardComponent from 'src/shared/ui/layout/PPLCardComponent.vue';
-import BodyComponent from 'src/shared/ui/layout/BodyComponent.vue';
-import { ref } from 'vue';
 
 const route = useRoute();
 const titulo = computed(() => {
   const title = route.meta.title;
-  return typeof title === 'string' ? title : 'Home';
+  return typeof title === 'string' ? title : 'Incidencias';
 });
+
+const centroActual: Centro = {
+  id: 1,
+  nombre_centro: 'CEFERESO No. 1',
+  codigo: 'EMX001',
+  nomenclatura_centro: 'CFRS1/AJ/',
+  consecutivo_inicio: 6201,
+  entidad_federativa: 15,
+  municipio: 5,
+  tipo: 'M',
+  created_at: '2024-08-14T19:40:52.000000Z',
+  updated_at: null,
+  deleted_at: null,
+  nombre_completo: 'CENTRO FEDERAL DE REINSERCIÓN SOCIAL NÚM. 1 "ALTIPLANO"',
+  numero: 1,
+  entrada: 'garitas',
+};
+
+const user: User = {
+  name: 'SUPER JS',
+  tipo: 'SUPER ADMINISTRADOR',
+  role: { name: 'SUPER ADMINISTRADOR' },
+  photo: '', // opcional
+};
 
 const isPPL = ref(true);
 </script>
-<style scoped></style>
