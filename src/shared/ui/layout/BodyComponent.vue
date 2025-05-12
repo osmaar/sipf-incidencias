0b748c26ad9cefa2e2322c9adc04aa29d03f8c8f
Osmar López Islas <osmichan07@gmail.com>
2025-05-12 15:00:20 -0600

    Reemplazar q-card-body por q-card-section en BodyComponent para mejorar la semántica del componente

--- src/shared/ui/layout/BodyComponent.vue
+++ src/shared/ui/layout/BodyComponent.vue
@@ -1,11 +1,11 @@
 <template>
   <q-card class="q-ma-md q-pa-md">
-    <q-card-body>
+    <q-card-section>
       <q-btn icon="keyboard_backspace" @click="redirectSipf" flat class="tw-text-emerald-900"
         >Regresar Home SIPF</q-btn
       >
       <router-view />
-    </q-card-body>
+    </q-card-section>
   </q-card>
 </template>
 
