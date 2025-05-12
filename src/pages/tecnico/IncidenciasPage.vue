f8e9640b866cfcf20aa57dbccf35175f2acf98b2
Osmar López Islas <osmichan07@gmail.com>
2025-05-12 15:00:03 -0600

    Actualizar la página de Incidencias Técnico para mejorar la estructura y el estilo del componente

--- src/pages/tecnico/IncidenciasPage.vue
+++ src/pages/tecnico/IncidenciasPage.vue
@@ -1,9 +1,13 @@
 <template>
-  <div>
-    <p class="tw-text-red-400">Incidencias tecnico</p>
-  </div>
+  <h1>Incidencias Técnico</h1>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+console.log('Página de Incidencias Técnico cargada');
+</script>
 
-<style scoped></style>
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
