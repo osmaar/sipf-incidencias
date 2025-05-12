c4bafdd82fd244e744702dba98c6cb44a87aee07
Osmar López Islas <osmichan07@gmail.com>
2025-05-12 15:00:27 -0600

    Eliminar tabs innecesarios en MenuComponent para simplificar la navegación

--- src/shared/ui/layout/MenuComponent.vue
+++ src/shared/ui/layout/MenuComponent.vue
@@ -1,9 +1,6 @@
 <template>
   <q-tabs align="center" shrink class="text-white q-ml-xl">
-    <q-route-tab to="/sanciones-tecnico" label="Sanciones - técnico" />
-    <q-route-tab to="/sanciones-juridico" label="Sanciones - jurídico" />
-    <q-route-tab to="/incidencias-tecnico" label="Incidencias - técnico" />
-    <q-route-tab to="/incidencias-juridico" label="Incidencias - jurídico" />
+    <q-route-tab to="/incidentes-tecnico" label="Incidencias" />
   </q-tabs>
 </template>
 
