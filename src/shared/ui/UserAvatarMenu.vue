b081eb12d8a8964960762fdc548de063542011ea
Osmar López Islas <osmichan07@gmail.com>
2025-05-12 15:02:40 -0600

    Eliminar importación innecesaria de defineProps y defineEmits en UserAvatarMenu.vue

--- src/shared/ui/UserAvatarMenu.vue
+++ src/shared/ui/UserAvatarMenu.vue
@@ -33,7 +33,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, defineProps, defineEmits } from 'vue';
+import { ref } from 'vue';
 import type { User } from 'src/entities/user/user.model';
 
 const props = defineProps<{ user: User }>();
