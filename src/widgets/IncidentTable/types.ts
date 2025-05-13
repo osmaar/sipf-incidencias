c359e915c2dd6358c8bb672df4a83ee98ef858b5
Osmar López Islas <osmichan07@gmail.com>
2025-05-13 14:23:57 -0600

    Agregar interfaz IncidentTableCardItem para definir la estructura de datos de las incidencias.

---
+++ src/widgets/IncidentTable/types.ts
@@ -0,0 +1,8 @@
+export interface IncidentTableCardItem {
+  folio: string;
+  fechaHora: string;
+  tipoIncidencia: string;
+  personalCustodia: string;
+  cargoPersonal: string;
+  estatus: string;
+}
