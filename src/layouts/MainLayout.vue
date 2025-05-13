1e6f74d2fb7ec55b2b871dcea5c0075ee9717f94
Osmar López Islas <osmichan07@gmail.com>
2025-05-13 14:23:25 -0600

    Refactor MainLayout.vue para utilizar computed properties para centroActual y user, mejorando la gestión del estado y la claridad del código.

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <HeaderComponent :titulo="titulo" :centro="centroActual" :user="user" />
+    <HeaderComponent :titulo="titulo" v-if="user && centroActual" :centro="centroActual" :user="user" />
     <q-page-container>
       <div class="row">
         <div class="col-2" v-if="isPPL">
@@ -17,41 +17,33 @@
 <script setup lang="ts">
 import { ref, computed } from 'vue';
 import { useRoute } from 'vue-router';
+import { useSessionStore } from 'stores/session';
 import type { User } from 'src/entities/user/user.model';
 import type { Centro } from 'src/entities/centro/centro.model';
 import BodyComponent from 'src/shared/ui/layout/BodyComponent.vue';
 import HeaderComponent from 'src/shared/ui/layout/HeaderComponent.vue';
 import PPLCardComponent from 'src/shared/ui/layout/PPLCardComponent.vue';
 
+const session = useSessionStore();
 const route = useRoute();
 const titulo = computed(() => {
   const title = route.meta.title;
   return typeof title === 'string' ? title : 'Incidencias';
 });
 
-const centroActual: Centro = {
-  id: 1,
-  nombre_centro: 'CEFERESO No. 1',
-  codigo: 'EMX001',
-  nomenclatura_centro: 'CFRS1/AJ/',
-  consecutivo_inicio: 6201,
-  entidad_federativa: 15,
-  municipio: 5,
-  tipo: 'M',
-  created_at: '2024-08-14T19:40:52.000000Z',
-  updated_at: null,
-  deleted_at: null,
-  nombre_completo: 'CENTRO FEDERAL DE REINSERCIÓN SOCIAL NÚM. 1 "ALTIPLANO"',
-  numero: 1,
-  entrada: 'garitas',
-};
+const centroActual = computed(() => {
+  if (!session.centro) {
+    throw new Error('❌ No se ha cargado session.centro');
+  }
+  return session.centro as Centro;
+});
 
-const user: User = {
-  name: 'SUPER JS',
-  tipo: 'SUPER ADMINISTRADOR',
-  role: { name: 'SUPER ADMINISTRADOR' },
-  photo: '', // opcional
-};
+const user = computed(() => {
+  if (!session.usuario) {
+    throw new Error('❌ No se ha cargado session.usuario');
+  }
+  return session.usuario as User;
+});
 
 const isPPL = ref(true);
 </script>
