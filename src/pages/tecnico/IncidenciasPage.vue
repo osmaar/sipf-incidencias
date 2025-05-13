d98f91a3a6a5114ace09e3447b1a59794882f88c
Osmar López Islas <osmichan07@gmail.com>
2025-05-13 14:23:28 -0600

    Reestructurar IncidenciasPage.vue para incluir la vista de incidencias con IncidentTableCardView, mejorando la presentación y funcionalidad de la página.

--- src/pages/tecnico/IncidenciasPage.vue
+++ src/pages/tecnico/IncidenciasPage.vue
@@ -1,13 +1,33 @@
-<template>
-  <h1>Incidencias Técnico</h1>
-</template>
-
 <script setup lang="ts">
-console.log('Página de Incidencias Técnico cargada');
+import { IncidentTableCardView, type IncidentTableCardItem } from 'src/widgets/IncidentTable';
+
+const items: IncidentTableCardItem[] = [
+  {
+    folio: 'INC/CFRS15/VC/000001/2025',
+    fechaHora: '12/05/2025 16:39',
+    tipoIncidencia: 'AGRESIÓN VERBAL A PPL',
+    personalCustodia: 'CHANCHITO FELIZ',
+    cargoPersonal: 'CARGADOR DE MASETAS',
+    estatus: 'REGISTRADO',
+  },
+];
+
+function onView(item: IncidentTableCardItem) {
+  console.log('View', item);
+}
+
+function onPrint(item: IncidentTableCardItem) {
+  console.log('Print', item);
+}
+
+function onEdit(item: IncidentTableCardItem) {
+  console.log('Edit', item);
+}
 </script>
 
-<style scoped>
-h1 {
-  color: #42b983;
-}
-</style>
+<template>
+  <div>
+    <h1 class="text-xl font-bold mb-4">Listado de Incidencias</h1>
+    <IncidentTableCardView :items="items" @view="onView" @print="onPrint" @edit="onEdit" />
+  </div>
+</template>
