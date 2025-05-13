8090b8fcd267e3f57257db14fb31dcc447d018bc
Osmar López Islas <osmichan07@gmail.com>
2025-05-13 14:23:10 -0600

    Modificar tipos de datos en la interfaz Expediente para mejorar la consistencia y claridad

--- src/entities/expediente/expediente.model.ts
+++ src/entities/expediente/expediente.model.ts
@@ -1,27 +1,73 @@
-import type { Centro } from '../centro/centro.model';
-
 export interface Expediente {
-  _type: string;
   id: number;
   persona_id: number;
   centro_id: number;
   num_expediente: string;
   consecutivo: number;
   cib: string;
-  cib_si_no: string | null;
-  folio_resguardo: string;
+  cib_si_no: boolean | null;
+  folio_resguardo: string | null;
   institucion_internacional: string | null;
   abrir_procesos: number;
-  readonly_biometrico: number | null;
-  readonly_senia: number | null;
-  readonly_info: number;
+  readonly_biometrico: boolean | null;
+  readonly_senia: number;
+  readonly_info: boolean | null;
   estatus: string;
   estatus_centro: string;
-  estatus_proceso: string;
+  estatus_proceso: string | null;
   readonly: number;
   autorizacion_expediente: number;
   created_at: string;
   updated_at: string;
   deleted_at: string | null;
   centro: Centro;
+  egreso_temporal?: { id?: number; descripcion?: string }[] | null;
+  traslado?: { id?: number; descripcion?: string }[] | null;
+  movimientos: Movimiento[];
+  proceso: Proceso;
+}
+
+export interface Centro {
+  id: number;
+  nombre_centro: string;
+  codigo: string;
+  nomenclatura_centro: string;
+  consecutivo_inicio: number;
+  entidad_federativa: number;
+  municipio: number;
+  tipo: string;
+  created_at: string;
+  updated_at: string;
+  deleted_at: string | null;
+  nombre_completo: string;
+  numero: number;
+  entrada: string;
+}
+
+export interface Movimiento {
+  id: number;
+  expediente_id: number;
+  tipo_ingreso: number;
+  es_nuevo_ingreso: number;
+  tipo_ingreso_desc: number;
+  procedencia_pais: string | null;
+  institucion_internacional: string | null;
+  folio_resguardo: string | null;
+  procedencia_centro: string | null;
+  procedencia_estado: string | null;
+  fiscalia: string | null;
+  fecha_ingreso: string;
+  created_at: string;
+  updated_at: string;
+  deleted_at: string | null;
+  circuito: string | null;
+  autoridad_judicial: string | null;
+  centro_destino: string;
+  fuero: string | null;
+  autoridad_judicial_otro: string | null;
+  circuito_estado_id: number | null;
+}
+
+export interface Proceso {
+  id: number;
 }
