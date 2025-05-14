8b90b2f2edba85b8c8a373d494aefc8e787ad3e6
Osmar López Islas <osmichan07@gmail.com>
2025-05-14 14:50:18 -0600

    Agregar componente ErrorForbidden.vue para manejar errores de acceso sin permisos

---
+++ src/pages/errores/ErrorForbidden.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="fullscreen bg-primary text-white text-center q-pa-md flex flex-center">
+    <div>
+      <div style="font-size: 30vh">404</div>
+
+      <div class="text-h5 q-mt-md">
+        No tienes los permisos necesarios para acceder a esta vista.
+      </div>
+      <q-btn class="q-mt-xl" color="secondary" text-color="white" unelevated @click="redirectSipf"
+        label="Regresar a SIPF" no-caps />
+
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { appConfig } from 'src/shared/config/app.config';
+
+function redirectSipf() {
+  const urlSipf = appConfig.frontendUrl;
+  if (!urlSipf) {
+    console.error('❌ URL de frontend no encontrada para el entorno actual');
+    return;
+  }
+  window.location.href = urlSipf;
+}
+
+</script>
