0c8ac180b8efe2bc434544ac210006c7d3df80ea
Osmar López Islas <osmichan07@gmail.com>
2025-05-14 16:06:52 -0600

    Eliminar el componente IncidenciasTable y sus tipos asociados para simplificar la estructura del proyecto.

--- src/widgets/IncidentTable/types.ts
+++
@@ -1,8 +0,0 @@
-export interface IncidentTableCardItem {
-  folio: string;
-  fechaHora: string;
-  tipoIncidencia: string;
-  personalCustodia: string;
-  cargoPersonal: string;
-  estatus: string;
-}
