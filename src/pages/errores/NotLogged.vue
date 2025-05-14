b50661d8e6348659a3b82453e51c0057d0d288ba
Osmar López Islas <osmichan07@gmail.com>
2025-05-14 14:50:25 -0600

    Agregar componente NotLogged.vue para manejar errores de acceso no autorizado

---
+++ src/pages/errores/NotLogged.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="fullscreen bg-primary text-white text-center q-pa-md flex flex-center">
+    <div>
+      <div style="font-size: 30vh">403</div>
+
+      <div class="text-h5 q-mt-md">
+        Acceso denegado. No tienes un token válido, porfavor inicia Sesión.
+      </div>
+      <q-btn class="q-mt-xl" color="secondary" text-color="white" unelevated @click="redirectSipf" label="Ir a SIPF"
+        no-caps />
+
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { appConfig } from 'src/shared/config/app.config';
+
+function redirectSipf() {
+  const urlSipf = appConfig.frontendUrl;
+  if (!urlSipf) {
+    console.error('❌ URL de frontend no encontrada para el entorno actual');
+    return;
+  }
+  window.location.href = urlSipf;
+}
+
+</script>
