645523c90c01d7aedb3a162a0a0ff5c3050afa84
Osmar López Islas <osmichan07@gmail.com>
2025-05-14 10:06:33 -0600

    Refactor appConfig: encapsular la obtención de variables de entorno en una función para mejorar la legibilidad y manejo de errores

--- src/shared/config/app.config.ts
+++ src/shared/config/app.config.ts
@@ -1,10 +1,17 @@
 const currentEnv = import.meta.env.VITE_APP_ENV ?? 'LOCAL';
 
+function getEnvVar(key: string): string {
+  const value = import.meta.env[key];
+  if (!value) {
+    console.error(`⚠️ No se encontró la variable de entorno: ${key}`);
+  }
+  return value ?? '';
+}
+
 export const appConfig = {
-  frontendUrl: import.meta.env[`VITE_APP_FRONTEND_${currentEnv}`],
-  apiUrl: import.meta.env[`VITE_APP_API_URL_${currentEnv}`],
-  storageUrl: import.meta.env[`VITE_API_STORAGE_URL_${currentEnv}`],
-  sancionesUrl: import.meta.env[`VITE_APP_SANCIONES_URL_${currentEnv}`],
-  loginUrl: import.meta.env[`VITE_APP_FRONTEND_LOGIN_URL_${currentEnv}`],
-  logoutEndpoint: import.meta.env[`VITE_APP_API_URL_${currentEnv}`] + '/logout',
+  frontendUrl: getEnvVar(`VITE_APP_FRONTEND_${currentEnv}`),
+  apiUrl: getEnvVar(`VITE_APP_API_URL_${currentEnv}`),
+  storageUrl: getEnvVar(`VITE_API_STORAGE_URL_${currentEnv}`),
+  loginUrl: getEnvVar(`VITE_APP_FRONTEND_LOGIN_URL_${currentEnv}`) + '/sipf/account/login',
+  logoutEndpoint: getEnvVar(`VITE_APP_API_URL_${currentEnv}`) + '/logout',
 };
