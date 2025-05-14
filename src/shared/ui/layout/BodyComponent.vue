3d8669f62ccd365a5587ec6774395f8a177e1b92
Osmar López Islas <osmichan07@gmail.com>
2025-05-13 18:39:44 -0600

    Refactor BodyComponent.vue: mejorar diseño del card y eliminar función de redirección innecesaria

--- src/shared/ui/layout/BodyComponent.vue
+++ src/shared/ui/layout/BodyComponent.vue
@@ -1,17 +1,13 @@
 <template>
-  <q-card class="q-ma-md q-pa-md">
+  <q-card class="rounded-2xl  p-6 bg-white border border-gray-100"
+    style="border-radius: 10px; box-shadow: 0 10px 64px rgba(0, 0, 0, 0.2), 0 6px 10px rgba(0, 0, 0, 0.14), 0 10px 3px -1px rgba(0, 0, 0, 0.12);">
+
     <q-card-section>
-      <q-btn icon="keyboard_backspace" @click="redirectSipf" flat class="tw-text-emerald-900"
-        >Regresar Home SIPF</q-btn
-      >
       <router-view />
     </q-card-section>
   </q-card>
 </template>
 
 <script setup lang="ts">
-function redirectSipf() {
-  const urlSipf = import.meta.env.VITE_APP_FRONTEND;
-  window.location.href = urlSipf;
-}
+
 </script>
